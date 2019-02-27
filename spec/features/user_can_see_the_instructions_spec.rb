a572aba5f0a0808811e03be61f87f0f89f5363e7
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-26 20:51:56 -0700

    create test for instruction page

---
+++ spec/features/user_can_see_the_instructions_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "User visits the instructions page" do
+  it "sees " do
+    visit "/"
+
+    click_on "Instructions"
+
+    expect(page).to have_content("How to Play")
+
+    click_on "Home"
+
+    expect(current_path).to eq("/")
+  end
+end
